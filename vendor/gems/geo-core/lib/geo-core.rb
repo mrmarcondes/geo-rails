78a407c9c26a3a6601a55e731ace6b16ed21d64a
Marco Rojo <mrmarcondes@gmail.com>
2013-02-11 14:57:36 -0200

    teste

--- vendor/gems/geo-core/lib/geo-core.rb
+++ vendor/gems/geo-core/lib/geo-core.rb
@@ -4,5 +4,6 @@ require 'geo-core/domain/item_orcamento'
 require 'geo-core/service/orcamento_service'
 
 current_path = File.dirname(__FILE__)
+puts "aaaaaaaa"
 puts current_path
 Mongoid.load!(current_path << "/mongoid.yml", :production)
